887a5bc227c881bcfc096f08ef16237cb0632763
Gavin.Lin <whatever.2099@gmail.com>
2017-12-13 08:59:31 +0800

    add more website

--- common.py
+++ common.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import io
+import gzip
 from random import choice
 
 BASEHEADERS = {'binanceProduct':{'method': 'GET',
@@ -27,10 +29,22 @@ BASEHEADERS = {'binanceProduct':{'method': 'GET',
                    	 	'If-None-Match':'W/"b93c86acbabfbc396f5677c6605012ac"',
                    	 	'Upgrade-Insecure-Requests':'1'
                    	 	},
+                'kuaidaili':{
+                    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding':'gzip, deflate',
+                    'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6,de;q=0.5',
+                    'Cache-Control':'max-age=0',
+                    'Connection':'keep-alive',
+                    'Cookie':'yd_cookie=feba52a3-03f6-45cd508dba336e0c909c630df298a92d269c; _gat=1; _ga=GA1.2.1993143436.1512804818; _gid=GA1.2.1168630916.1512804818; Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31=1512804820; Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31=1512804975',
+                    'Host':'www.kuaidaili.com',
+                    'Upgrade-Insecure-Requests':'1',
+
+                },
                    }
 PCUSERAGENTS =  [
-    "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-    "Avant Browser/1.2.789rel1 (http://www.avantbrowser.com)",
+    # "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+    # "Avant Browser/1.2.789rel1 (http://www.avantbrowser.com)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
     "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5",
     "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/532.9 (KHTML, like Gecko) Chrome/5.0.310.0 Safari/532.9",
     "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.514.0 Safari/534.7",
@@ -41,24 +55,24 @@ PCUSERAGENTS =  [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
     "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.0 x64; en-US; rv:1.9pre) Gecko/2008072421 Minefield/3.0.2pre",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.11) Gecko/2009060215 Firefox/3.0.11 (.NET CLR 3.5.30729)",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 GTB5",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; tr; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 ( .NET CLR 3.5.30729; .NET4.0E)",
-    "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-    "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110622 Firefox/6.0a2",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0b4pre) Gecko/20100815 Minefield/4.0b4pre",
-    "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0 )",
-    "Mozilla/4.0 (compatible; MSIE 5.5; Windows 98; Win 9x 4.90)",
-    "Mozilla/5.0 (Windows; U; Windows XP) Gecko MultiZilla/1.6.1.0a",
-    "Mozilla/2.02E (Win95; U)",
-    "Mozilla/3.01Gold (Win95; I)",
-    "Mozilla/4.8 [en] (Windows NT 5.1; U)",
-    "Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
+    # "Mozilla/5.0 (Windows; U; Windows NT 6.0 x64; en-US; rv:1.9pre) Gecko/2008072421 Minefield/3.0.2pre",
+    # "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10",
+    # "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.11) Gecko/2009060215 Firefox/3.0.11 (.NET CLR 3.5.30729)",
+    # "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 GTB5",
+    # "Mozilla/5.0 (Windows; U; Windows NT 5.1; tr; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 ( .NET CLR 3.5.30729; .NET4.0E)",
+    # "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    # "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    # "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+    # "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110622 Firefox/6.0a2",
+    # "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1",
+    # "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0b4pre) Gecko/20100815 Minefield/4.0b4pre",
+    # "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0 )",
+    # "Mozilla/4.0 (compatible; MSIE 5.5; Windows 98; Win 9x 4.90)",
+    # "Mozilla/5.0 (Windows; U; Windows XP) Gecko MultiZilla/1.6.1.0a",
+    # "Mozilla/2.02E (Win95; U)",
+    # "Mozilla/3.01Gold (Win95; I)",
+    # "Mozilla/4.8 [en] (Windows NT 5.1; U)",
+    # "Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
 ]
 
 MOBILEUSERAGENTS= [
@@ -110,6 +124,7 @@ def getRandomHeaders(webName, userAgentsType='All'):
     baseHeaders['user-agent'] = userAgent
     return baseHeaders
 
+
 def getRandomUserAgents(userAgentsType='All'):
     if userAgentsType == 'PC':
         return choice(PCUSERAGENTS)
@@ -118,6 +133,26 @@ def getRandomUserAgents(userAgentsType='All'):
     else:
         allUserAgents = PCUSERAGENTS + MOBILEUSERAGENTS
         return choice(allUserAgents)
+
+
+def gzip_str(string_):
+    out = io.BytesIO()
+
+    with gzip.GzipFile(fileobj=out, mode='w') as fo:
+        fo.write(string_.encode())
+
+    bytes_obj = out.getvalue()
+    return bytes_obj
+
+
+def gunzip_bytes_obj(bytes_obj):
+    in_ = io.BytesIO()
+    in_.write(bytes_obj)
+    in_.seek(0)
+    with gzip.GzipFile(fileobj=in_, mode='rb') as fo:
+        gunzipped_bytes_obj = fo.read()
+
+    return gunzipped_bytes_obj.decode()
 
 
 if __name__ == '__main__':
--- proxy.py
+++ proxy.py
@@ -1,16 +1,115 @@
 # -*- coding: utf-8 -*-
 
 import common
+from re import compile
+from json import loads
+# from socket import timeout
 from urllib import request
 from random import choice
 from time import sleep
 from bs4 import BeautifulSoup
 
+
 class proxy(object):
-    proxyUrls = ['http://www.xicidaili.com/']
+    # proxyUrls = ['http://www.xicidaili.com/']
+    # proxyUrls = {'ip3366':'http://www.ip3366.net/free/?stype=2', 'xici':'http://www.xicidaili.com/', '89ip':'http://www.89ip.cn/tiqv.php?sxb=&tqsl=30&ports=&ktip=&xl=on&submit=%CC%E1++%C8%A1', 'xdaili':'http://www.xdaili.cn/ipagent//freeip/getFreeIps?page=1&rows=10', 'kuaidaili':'http://www.kuaidaili.com/free/intr/', 'cnproxy':'http://cn-proxy.com/'}
+    proxyUrls = {'kuaidaili':'http://www.kuaidaili.com/free/intr/'}
     testUrl = 'http://icanhazip.com/'
-    proxyList = []
+    proxyDict = {}
     checking = False
+
+    def __getSpecifyUrl(self, webSite, url):
+        headers = {}
+        ipDict = {}
+        headers['user-agent'] = common.getRandomUserAgents('PC')
+        if webSite == 'kuaidaili':
+            headers = common.getRandomHeaders('kuaidaili', 'PC')
+            
+        try:
+            req = request.Request(url, headers=headers)
+            res = request.urlopen(req, timeout=3)
+        except Exception as e:
+            print('%s error happend when open %s with %s user_agent' % (e, url, headers))
+            return ipDict
+
+        content = res.read()
+        soup = BeautifulSoup(content, "lxml")
+        if webSite == 'xici':
+            ipInfoList = soup.find_all('tr', attrs={'class':'odd'})
+            for ipInfo in ipInfoList:
+                proxyType = ipInfo.find_all('td')[4].get_text()
+                if proxyType != '透明':
+                    continue
+                httpType = ipInfo.find_all('td')[5].get_text()
+                # if httpType != 'HTTP':
+                #     continue
+                ip = ipInfo.find_all('td')[1].get_text()
+                try:
+                    port = int(ipInfo.find_all('td')[2].get_text())
+                except ValueError:
+                    continue
+                proxy = '%s:%d' % (ip, port)
+                ipDict[proxy] = httpType
+
+        elif webSite == 'ip3366':
+            print('go to ip3366')
+            divTag = soup.find_all('div', attrs={'id':'list', 'style':'margin-top:15px;'})[0]
+            trResult = divTag.find_all('tr')
+            for tr in trResult:
+                try:
+                    ip = tr.find_all('td')[0].get_text()
+                    port = tr.find_all('td')[1].get_text()
+                    port = int(port)
+                    httpType = tr.find_all('td')[3].get_text()
+                except (IndexError, ValueError):
+                    continue
+                proxy = '%s:%d' % (ip, port)
+                ipDict[proxy] = httpType
+
+        elif webSite == '89ip':
+            print('go to 89ip')
+            proxyPat = compile(r'(\d+\.\d+\.\d+\.\d+:\d+)')
+            fixedContent = soup.prettify()
+            lines = fixedContent.split('\n')
+            for line in lines:
+                line = line.strip(' ')
+                match = proxyPat.match(line)
+                if not match:
+                    continue
+                proxy = match.group(0)
+                ipDict[proxy] = 'http'
+
+        elif webSite == 'xdaili':
+            content = content.decode('utf8')
+            jsonContent = loads(content)
+            ipProxies = jsonContent['RESULT']['rows']
+            for ipProxy in ipProxies:
+                ip = ipProxy['ip']
+                try:
+                    port = int(ipProxy['port'])
+                except ValueError:
+                    continue
+                proxy = '%s:%d' % (ip, port)
+                ipDict[proxy] = 'http'
+
+        elif webSite == 'kuaidaili':
+            unGzipContent = common.gunzip_bytes_obj(content)
+            soup = BeautifulSoup(unGzipContent, "lxml")
+            divResult = soup.find_all('div', attrs={'id':'list', 'style':'margin-top:15px;'})
+            divTag = divResult[0]
+            trResult = divTag.find_all('tr')
+            for tr in trResult:
+                try:
+                    ip = tr.find_all('td')[0].get_text()
+                    port = int(tr.find_all('td')[1].get_text())
+                    httpType = tr.find_all('td')[3].get_text()
+                except (ValueError, IndexError):
+                    continue
+                proxy = '%s:%d' % (ip, port)
+                ipDict[proxy] = httpType
+
+        print('allUncheckProxy:', ipDict)
+        return ipDict
 
     def __getProxy(self):
         if self.checking == True:
@@ -18,94 +117,68 @@ class proxy(object):
             sleep(60)
             return
         self.checking = True
-        headers = {}
-        unCheckproxyList = []
-        # headers = common.getRandomHeaders('xici', 'PC')
-        headers['user-agent'] = common.getRandomUserAgents()
-        for proxyUrl in self.proxyUrls:
-            try:
-                req = request.Request(proxyUrl, headers=headers)
-                res = request.urlopen(req, timeout=3)
-            except:
-                print('Can not open %s, removed' % proxyUrl)
-                self.proxyUrls.remove(proxyUrl)
-                continue
-            content = res.read()
-            soup = BeautifulSoup(content, "lxml")
-            ipInfoList = soup.find_all('tr', attrs={'class':'odd'})
-            for ipInfo in ipInfoList:
-                proxyType = ipInfo.find_all('td')[4].get_text()
-                if proxyType != '透明':
-                    continue
-                httpType = ipInfo.find_all('td')[5].get_text()
-                if httpType != 'HTTP':
-                    continue
+        unCheckProxyDict = {}
+        for key, value in self.proxyUrls.items():
+            # tmpProxyDict = self.__getSpecifyUrl(key, value)
+            unCheckProxyDict = dict(unCheckProxyDict, **self.__getSpecifyUrl(key, value))
 
-                ip = ipInfo.find_all('td')[1].get_text()
-                try:
-                    port = int(ipInfo.find_all('td')[2].get_text())
-                except ValueError:
-                    continue
-
-                proxy = '%s:%d' % (ip, port)
-                unCheckproxyList.append(proxy)
-        print("unCheckProxy:%d" % len(unCheckproxyList))
-        self.checkProxy(unCheckproxyList)
+        print('unCheckProxyDict:', len(unCheckProxyDict))
+        self.checkProxy(unCheckProxyDict)
 
     def startGetProxy(self):
         self.__getProxy()
 
 
-    def checkProxy(self, proxyList):
-        for proxy in proxyList:
-            choiceProxy = {'http': proxy}
+    def checkProxy(self, proxyDict):
+        for proxy, httpType in proxyDict.items():
+            choiceProxy = {httpType.lower(): proxy}
+            print('choiceProxy:', choiceProxy)
             proxy_support = request.ProxyHandler(choiceProxy)
             opener = request.build_opener(proxy_support)
             request.install_opener(opener)
             try:
                 #response = request.urlopen(self.testUrl, timeout=3)
                 response = request.urlopen(self.testUrl, timeout=5)
-            except:
-                print('%s proxy can not use, continue' % proxy)
+                content = response.read()
+            except Exception as e:
+                print('Error happened:%s. proxy %s:%s can not be used, continue' % (e, httpType, proxy))
                 continue
             ip = proxy.split(':')[0]
-            content = response.read()
             content = content.decode('utf8')
             content = content.strip('\n')
             print('content:%s, ip:%s' % (content, ip))
             if content == ip:
-                self.proxyList.append(proxy)
+                self.proxyDict[proxy] = httpType
         self.checking = False
-        if len(self.proxyList) == 0:
+        if len(self.proxyDict) == 0:
             sleep(60)
             self.__getProxy()
             return
-        print('Length:%d' % len(self.proxyList))
+        print('Length:%d' % len(self.proxyDict))
 
     def randomChoice(self):
-        # if len(self.proxyList) == 0:
-        #     self.__getProxy()
-        #     return self.randomChoice()
-        # return choice(self.proxyList)
-
         try:
-            return choice(self.proxyList)
+            return choice(list(self.proxyDict.items()))
         except IndexError:
             self.__getProxy()
             return self.randomChoice()
 
     def removeProxy(self, aproxy):
         try:
-            self.proxyList.remove(aproxy)
-        except ValueError:
+            self.proxyDict.pop(aproxy)
+        except KeyError:
             print('alread removed, ignore')
+
+    def showAllProxy(self):
+        print('allProxy:', self.proxyDict)
 
 
 if __name__ == "__main__":
     ipProxy = proxy()
     ipProxy.startGetProxy()
-    aproxy = ipProxy.randomChoice()
-    print('proxy:', aproxy)
+    ipProxy.showAllProxy()
+    # aproxy = ipProxy.randomChoice()
+    # print('proxy:', aproxy)
 
     #proxies = {'http' : '115.183.11.158:9999'}
     #url = 'http://icanhazip.com/'
